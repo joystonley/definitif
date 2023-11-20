bc1abfa714c995ab369f59079e2ee5c526603c5c
joystonley <111059822+joystonley@users.noreply.github.com>
2023-11-20 09:14:30 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -114,15 +114,13 @@ USE_TZ = True
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, 'apps/static'),
-)
 
 # settings.py
+
+# Définissez les répertoires pour les fichiers statiques et les fichiers médias
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
